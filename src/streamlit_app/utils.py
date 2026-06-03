9ced96e6f0678c84cc6c030e77e7f3bab4c6f422
julblanke <j_blan06@wwu.de>
2026-06-03 18:41:46 +0200

    misc: wip

--- src/streamlit_app/utils.py
+++ src/streamlit_app/utils.py
@@ -1,52 +1,58 @@
 from typing import List, Dict
 
-
-IMG_LENA = "https://www.rtl.de/img/4370049/1747732080/c16_9/1024/bachelor-kandidatin-lena.webp"
-IMG_CLARA = "https://www.rtl.de/img/4370043/1747743223/c16_9/1024/bachelor-kandidatin-clara.webp"
-IMG_SEYMA = "https://www.rtl.de/img/4370071/1747732936/c16_9/1024/bachelor-kandidatin-seyma.webp"
-IMG_AYLIN = "https://www.rtl.de/img/4370182/1747743206/c16_9/1024/bachelor-kandidatin-aylin.webp"
-IMG_PAULINA = "https://www.rtl.de/img/4370502/1747821770/c16_9/1024/49747-2jpg.webp"
-IMG_VIVIANE = "https://www.rtl.de/img/4370080/1747821560/c16_9/1024/1c958-3jpg.webp"
-IMG_ANN_KATHRIN = "https://www.rtl.de/img/4370030/1747731438/c16_9/1024/bachelor-kandidatin-ann-kathrin.webp"
-IMG_LEONIE = "https://www.rtl.de/img/4370053/1747732222/c16_9/1024/bachelorkandidatin-leonie.webp"
-IMG_LOUISA = "https://www.rtl.de/img/4370552/1747743628/c16_9/1024/louisa.webp"
-IMG_NADINE = "https://www.rtl.de/img/4370494/1747821737/c16_9/1024/87137-2jpg.webp"
-IMG_ISABELLE = "https://www.rtl.de/img/4370471/1747741941/c16_9/1024/bachelor-kandidatin-isabelle.webp"
-IMG_HANNAH = "https://www.rtl.de/img/4370542/1747743408/c16_9/1024/bachelor-kandidatin-hannah.webp"
+IMG_KATALINA = "https://www.rtl.de/img/30717395/1776267338/o/1200/1200/katalina11529.webp"
+IMG_JOSI = "https://www.rtl.de/img/30717391/1776267545/o/1200/1200/josi4b1c5.webp"
+IMG_NADJA = "https://www.rtl.de/img/30717384/1776267127/o/1200/1200/nadja7ab76-1.webp"
+IMG_PAULINA = "https://www.rtl.de/img/30717398/1776267097/o/1200/1200/paulnaabd27.webp"
+IMG_KIM = "https://www.rtl.de/img/30717381/1776267074/o/1200/1200/kimjulia93932-1.webp"
+IMG_VIVIEN = "https://www.rtl.de/img/30717400/1776265326/o/1200/1200/vivibc59c-1.webp"
+IMG_JANETTE = "https://www.rtl.de/img/30717387/1776267465/o/1200/1200/janettec67ce.webp"
+IMG_MICHELLE = "https://www.rtl.de/img/30717390/1776267433/o/1200/1200/michelle31577.webp"
+IMG_YANA = "https://www.rtl.de/img/30717379/1776267150/o/1200/1200/yana73715.webp"
+IMG_NATALIE = "https://www.rtl.de/img/30717383/1776267213/o/1200/1200/natalie7d0ff.webp"
+IMG_LAURA = "https://www.rtl.de/img/30717393/1776267318/o/1200/1200/laura7b097.webp"
+IMG_KATARINA = "https://www.rtl.de/img/30717389/1776267524/o/1200/1200/katarinac9a7f.webp"
+IMG_JULIANA = "https://www.rtl.de/img/30717386/1776267289/o/1200/1200/juleac5ef.webp"
 IMG_PLACEHOLDER = "https://i.pinimg.com/736x/d3/4e/c5/d34ec52e51bef130bb5f881daed91a8d.jpg"
-
+IMG_LIA = "https://www.rtl.de/img/30717404/1776267364/o/1200/1200/liae0383.webp"
+IMG_CHIARA = "https://www.rtl.de/img/30717396/1776267500/o/1200/1200/chiara6172b.webp"
+IMG_SEBASTIAN = "https://www.google.com/url?sa=t&source=web&rct=j&url=https%3A%2F%2Fwww.instagram.com%2Fitssebastianpaul%2F&ved=0CBYQjRxqFwoTCKCrrum665QDFQAAAAAdAAAAABAF&opi=89978449"
+IMG_TIM = "https://www.google.com/imgres?q=bachelor%20tim%20%202026&imgurl=https%3A%2F%2Fi0.web.de%2Fimage%2F910%2F42051910%2Cpd%3D1%2Cf%3Dsize-l%2Ftim-reitz.jpg&imgrefurl=https%3A%2F%2Fweb.de%2Fmagazine%2Funterhaltung%2Fthema%2Ftim-reitz&docid=TwHQ9Nsy5w6iyM&tbnid=DsqhNb7x7uf-6M&vet=12ahUKEwjXv_30uuuUAxUjgf0HHRIpCvMQnPAOegQIKhAB..i&w=543&h=680&hcb=2&ved=2ahUKEwjXv_30uuuUAxUjgf0HHRIpCvMQnPAOegQIKhAB"
 
 TEAM_IMAGES: Dict[str, List[str]] = {
-    "Valentin": [IMG_LENA, IMG_CLARA, IMG_SEYMA, IMG_AYLIN, IMG_PAULINA, IMG_VIVIANE],
-    "Julian":   [IMG_ANN_KATHRIN, IMG_VIVIANE, IMG_PAULINA, IMG_CLARA, IMG_LEONIE, IMG_LOUISA],
-    "Anja":     [IMG_LENA, IMG_CLARA, IMG_SEYMA, IMG_NADINE, IMG_AYLIN, IMG_ISABELLE],
-    "Josy":     [IMG_SEYMA, IMG_LENA, IMG_AYLIN, IMG_VIVIANE, IMG_ISABELLE, IMG_NADINE],
-    "Luisa":    [IMG_ISABELLE, IMG_LENA, IMG_VIVIANE, IMG_AYLIN, IMG_NADINE, IMG_LEONIE],
-    "Marie":    [IMG_LENA, IMG_SEYMA, IMG_VIVIANE, IMG_NADINE, IMG_PAULINA, IMG_CLARA],
-    "Mert":     [IMG_ANN_KATHRIN, IMG_AYLIN, IMG_HANNAH, IMG_LENA, IMG_NADINE, IMG_SEYMA],
-    "Rabea":    [IMG_PLACEHOLDER] * 6,
+    "Anja":     [IMG_KATALINA, IMG_JOSI, IMG_NADJA, IMG_PAULINA, IMG_KIM, IMG_VIVIEN, IMG_SEBASTIAN],
+    "Josefine": [IMG_NADJA, IMG_JANETTE, IMG_MICHELLE, IMG_YANA, IMG_KATALINA, IMG_VIVIEN],
+    "Marie":    [IMG_NATALIE, IMG_VIVIEN, IMG_NADJA, IMG_LAURA, IMG_YANA, IMG_KATALINA],
+    "Julian":   [IMG_NADJA, IMG_JANETTE, IMG_LAURA, IMG_KATALINA, IMG_LIA, IMG_JOSI, IMG_TIM],
+    "Mert":     [IMG_LIA, IMG_KATALINA, IMG_YANA, IMG_VIVIEN, IMG_KIM, IMG_PAULINA],
+    "Luisa":    [IMG_PLACEHOLDER, IMG_PLACEHOLDER, IMG_PLACEHOLDER, IMG_PLACEHOLDER, IMG_PLACEHOLDER, IMG_PLACEHOLDER],
+    "Anne":     [IMG_CHIARA, IMG_LIA, IMG_KATARINA, IMG_VIVIEN, IMG_YANA, IMG_NADJA],
 }
 
-
 IMAGE_NAME_MAP: Dict[str, str] = {
-    IMG_LENA: "Lena",
-    IMG_CLARA: "Clara",
-    IMG_SEYMA: "Seyma",
-    IMG_AYLIN: "Aylin",
+    IMG_KATALINA: "Katalina",
+    IMG_JOSI: "Josi",
+    IMG_NADJA: "Nadja",
     IMG_PAULINA: "Paulina",
-    IMG_VIVIANE: "Viviane",
-    IMG_ANN_KATHRIN: "Ann-Kathrin",
-    IMG_LEONIE: "Leonie",
-    IMG_LOUISA: "Louisa",
-    IMG_NADINE: "Nadine",
-    IMG_ISABELLE: "Isabelle",
-    IMG_HANNAH: "Hannah",
+    IMG_KIM: "Kim",
+    IMG_VIVIEN: "Vivien",
+    IMG_JANETTE: "Janette",
+    IMG_MICHELLE: "Michelle",
+    IMG_YANA: "Yana",
+    IMG_NATALIE: "Natalie",
+    IMG_LAURA: "Laura",
+    IMG_KATARINA: "Katarina",
+    IMG_JULIANA: "Julia",
+    IMG_LIA: "Lia",
+    IMG_CHIARA: "Chiara",
+    IMG_SEBASTIAN: "Sebastian",
+    IMG_TIM: "Tim",
     IMG_PLACEHOLDER: "TBA",
 }
 
 
 def get_team_images(person: str) -> List[str]:
     images = TEAM_IMAGES.get(person, [])
-    if len(images) < 6:
+    if len(images) < 7:
         images += [IMG_PLACEHOLDER] * (6 - len(images))
     return images
